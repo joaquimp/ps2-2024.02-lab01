e4c1e3a79e453afe152fa348dca6fd64b556a134
Joaquim Pessôa Filho <joaquimp@users.noreply.github.com>
2024-08-19 13:18:11 +0000

    exemplo em aula

---
+++ br/mack/ps2/Main.java
@@ -0,0 +1,13 @@
+package br.mack.ps2;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ola mundo");
+    }
+}
